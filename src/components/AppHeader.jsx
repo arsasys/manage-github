83f21c4fe371325daa18f5fac9ca8a96b6be79d0
arsasys <123162165+arsasys@users.noreply.github.com>
2025-03-20 20:15:03 +0800

    Update AppHeader.jsx

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { GithubOutlined } from '@ant-design/icons';
 
 const { Header } = Layout;
 
@@ -30,19 +31,9 @@ function AppHeader() {
   };
 
   return (
-    <Header style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%' }}>
-      <div
-        style={{
-          float: 'left',
-          width: 120,
-          height: 31,
-          margin: '16px 24px 16px 0',
-          color: 'white',
-          fontSize: '18px',
-          fontWeight: 'bold',
-        }}
-      >
-        GitHub管理器
+    <Header className="app-header">
+      <div className="app-logo">
+        <GithubOutlined /> GitHub管理器
       </div>
       <Menu
         theme="dark"
