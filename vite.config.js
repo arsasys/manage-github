f7a5df131f67bdab16c3a9042c968893300d6d5b
arsasys <123162165+arsasys@users.noreply.github.com>
2025-03-20 20:54:44 +0800

    Update vite.config.js
    确保刷新页面时不会出现404错误

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // 确保刷新页面时不会出现404错误
+  },
 })
